362546c2d0a35caf1b1c88c10dafc044d0b553a5
fishg <fishguy2468@gmail.com>
2024-09-26 15:09:53 -0400

    A Star complete and bugfixes

--- src/main/kotlin/AStar.kt
+++ src/main/kotlin/AStar.kt
@@ -1,3 +1,77 @@
-class AStar(val maze:Maze) {
+import java.util.PriorityQueue
+import kotlin.math.abs
 
+class AStar(val maze: Maze) {
+    val closed: HashSet<Tile> = HashSet()
+    private val open: PriorityQueue<Tile> = PriorityQueue(Tile::compareLargeG)
+
+    private var cur: Tile = maze.maze[maze.start.second][maze.start.first]
+
+    private fun h(t: Tile): Int {
+        return abs(maze.goal.x - t.x) + abs(maze.goal.y - t.y)
+    }
+
+    fun run() {
+        cur.f = Pair(h(cur), 1)
+        open.add(cur)
+        aStar()
+    }
+
+    private fun aStar() {
+        while (cur != maze.goal) {
+            closed.add(cur)
+
+            adjacent(cur).forEach { i ->
+                i.f = Pair(h(i), cur.f!!.second + 1)
+                open.add(i)
+            }
+            cur = open.remove()
+        }
+    }
+
+    private fun adjacent(t: Tile): List<Tile> {
+        val l = mutableListOf<Tile>()
+        if (t.x < 100 && !maze[t.y, t.x + 1].blocked && !closed.contains(maze[t.y, t.x + 1])) {
+            l.add(maze[t.y, t.x + 1])
+        }
+        if (t.y < 100 && !maze[t.y + 1, t.x].blocked && !closed.contains(maze[t.y + 1, t.x])) {
+            l.add(maze[t.y + 1, t.x])
+        }
+        if (t.x > 0 && !maze[t.y, t.x - 1].blocked && !closed.contains(maze[t.y, t.x - 1])) {
+            l.add(maze[t.y, t.x - 1])
+        }
+        if (t.y > 0 && !maze[t.y - 1, t.x].blocked && !closed.contains(maze[t.y - 1, t.x])) {
+            l.add(maze[t.y - 1, t.x])
+        }
+        return l
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        for (row in maze.maze) {
+            for (i in row) {
+                if (i.blocked) sb.append("#")
+                else if (i == maze[maze.start.second,maze.start.first]) sb.append("O")
+                else if (closed.contains(i)) sb.append("X")
+                else if (i == maze.goal) sb.append("!")
+                else sb.append("_")
+            }
+            sb.append("\n")
+        }
+        return sb.toString()
+    }
+}
+
+fun main() {
+    val m = Maze()
+    m.generateMaze()
+    println(m)
+    println("start: " + m.start)
+    println("goal: " +m.goal)
+    val a = AStar(m)
+    a.run()
+    println("astar")
+    println(a)
+
+    println("expanded: ${a.closed.size}")
 }
--- src/main/kotlin/Maze.kt
+++ src/main/kotlin/Maze.kt
@@ -1,21 +1,51 @@
 import kotlin.random.Random
 
-data class Tile(val x: Int, val y: Int, var visited: Boolean = false, var blocked: Boolean = false, var isGoal:Boolean = false)
+data class Tile(
+    val x: Int,
+    val y: Int,
+    var visited: Boolean = false,
+    var blocked: Boolean = false,
+    var isGoal: Boolean = false,
+    var f: Pair<Int,Int>? = null // (g,c)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other !is Tile) return false
+        return this.x == other.x && this.y == other.y
+    }
+
+    override fun toString(): String {
+        return "(${x},${y})"
+    }
+
+    companion object {
+        fun compareLargeG(a:Tile, b:Tile) :Int {
+            if (a.f!!.first + a.f!!.second == b.f!!.first + b.f!!.second) return  a.f!!.first.compareTo(b.f!!.first)
+            return (a.f!!.first + a.f!!.second).compareTo(b.f!!.first + b.f!!.second)
+        }
+
+        fun compareSmallG(a:Tile, b:Tile) : Int {
+            if (a.f!!.first + a.f!!.second == b.f!!.first + b.f!!.second) return -a.f!!.first.compareTo(b.f!!.first)
+            return (a.f!!.first + a.f!!.second).compareTo(b.f!!.first + b.f!!.second)
+        }
+    }
+
+
+}
 
 class Maze {
     val maze: Array<Array<Tile>> = Array(101) { i -> Array(101) { j -> Tile(j, i) } }
-    var goal: Pair<Int,Int> = Pair(0,0)
-    val start: Pair<Int,Int> = Pair(Random.nextInt(0,101),Random.nextInt(0,101))
+    var goal: Tile = Tile(-1,-1)
+    val start: Pair<Int, Int> = Pair(Random.nextInt(0, 101), Random.nextInt(0, 101))
 
     override fun toString(): String {
         val sb = StringBuilder()
-        for (r in maze){
+        for (r in maze) {
             for (i in r) {
                 if (i.blocked) {
                     sb.append("#")
-                } else if(i.isGoal) {
+                } else if (i.isGoal) {
                     sb.append("!")
-                } else if(i.x == start.first && i.y == start.second) {
+                } else if (i.x == start.first && i.y == start.second) {
                     sb.append("O")
                 } else {
                     sb.append("_")
@@ -25,44 +55,50 @@ class Maze {
         }
         return sb.toString()
     }
+
+    operator fun get(i:Int, j:Int) : Tile {
+        return maze[i][j]
+    }
+
     fun generateMaze() {
-        generateMaze(start.first,start.second)
-        var x = Random.nextInt(0,101)
-        var y = Random.nextInt(0,101)
-        while(!maze[y][x].visited) {
-            x = Random.nextInt(0,101)
-            y = Random.nextInt(0,101)
+        generateMaze(start.first, start.second)
+
+        var x = Random.nextInt(0, 101)
+        var y = Random.nextInt(0, 101)
+        while (!maze[y][x].visited || maze[y][x].blocked || Pair(x,y) == start) {
+            x = Random.nextInt(0, 101)
+            y = Random.nextInt(0, 101)
         }
         maze[y][x].isGoal = true
-        goal= Pair(x,y)
+        goal = maze[y][x]
     }
 
     private fun generateMaze(x: Int, y: Int) {
         maze[y][x].visited = true
-        var pM = calcPossibleNeighbors(x,y)
+        var pM = calcPossibleNeighbors(x, y)
         while (pM.isNotEmpty()) {
             val tileSelection = pM.random()
             if (Random.nextDouble() <= 0.3) {
                 tileSelection.blocked = true
                 tileSelection.visited = true
-            } else generateMaze(tileSelection.x,tileSelection.y)
-            pM = calcPossibleNeighbors(x,y)
+            } else generateMaze(tileSelection.x, tileSelection.y)
+            pM = calcPossibleNeighbors(x, y)
         }
     }
 
-    private fun calcPossibleNeighbors(x:Int, y:Int) : List<Tile> {
+    private fun calcPossibleNeighbors(x: Int, y: Int): List<Tile> {
         val t = mutableListOf<Tile>()
         if (x < 100 && !maze[y][x + 1].visited) {
-            t.add(maze[y][x+1])
+            t.add(maze[y][x + 1])
         }
-        if (y < 100 && !maze[y+1][x].visited) {
-            t.add(maze[y+1][x])
+        if (y < 100 && !maze[y + 1][x].visited) {
+            t.add(maze[y + 1][x])
         }
         if (x > 0 && !maze[y][x - 1].visited) {
-            t.add(maze[y][x-1])
+            t.add(maze[y][x - 1])
         }
-        if (y >0 && !maze[y-1][x].visited) {
-            t.add(maze[y-1][x])
+        if (y > 0 && !maze[y - 1][x].visited) {
+            t.add(maze[y - 1][x])
         }
         return t
     }
