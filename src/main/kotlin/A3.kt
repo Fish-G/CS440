7012ea4893404f29e4f354be2f2e56227e594a6c
Marco Hu <fishguy2468@gmail.com>
2024-11-02 23:42:12 -0400

    completion of assignment

--- src/main/kotlin/A3.kt
+++ src/main/kotlin/A3.kt
@@ -1,50 +1,48 @@
-import java.util.*
-import kotlin.collections.HashSet
 import kotlin.math.abs
 
 class A3 {
-    data class Tile(val id: Int, var v: Double)
-    data class State(val robot: Pair<Int, Int>, val bull: Pair<Int, Int>, var value: Double? = null) // (x,y)
+    data class State(
+        val robot: Pair<Int, Int>,
+        val bull: Pair<Int, Int>,
+        var value: Double? = null,
+        var id: Int
+    ) // (x,y)
 
-    val allStates = Array(12) { Array(12) { Array(12) { Array(12) { State(Pair(-1, -1), Pair(-1, -1)) } } } }
+    val allStates = Array(13) { Array(13) { Array(13) { Array(13) { State(Pair(-1, -1), Pair(-1, -1), id = -1) } } } }
 
     init {
+        var i = 0
         (0..12).forEach { rx ->
             (0..12).forEach { ry ->
                 (0..12).forEach { bx ->
                     (0..12).forEach { by ->
-                        allStates[rx][ry][bx][by] = State(Pair(rx, ry), Pair(bx, by))
+                        if (bx == 6 && by == 6) allStates[rx][ry][bx][by] =
+                            State(Pair(rx, ry), Pair(bx, by), value = 0.0, i)
+                        else allStates[rx][ry][bx][by] = State(Pair(rx, ry), Pair(bx, by), id = i)
+                        i++
                     }
                 }
             }
         }
     }
 
-    class StateSet() {
-        val rx = HashSet<Int>()
-        val ry = HashSet<Int>()
-        val bx = HashSet<Int>()
-        val by = HashSet<Int>()
-        fun add(s: State) {
-            rx.add(s.robot.first)
-            ry.add(s.robot.second)
-            bx.add(s.bull.first)
-            by.add(s.bull.second)
-        }
-
-        fun contains(s: State) =
-            rx.contains(s.robot.first) && ry.contains(s.robot.second) && bx.contains(s.bull.first) && by.contains(s.bull.second)
-    }
-
-
-    fun manhattan(a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
-    fun withinBounds(x: Int, y: Int) =
+    private fun manhattan(a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
+    private fun withinBounds(x: Int, y: Int) =
         (x in 0..12) && (y in 0..12) && !(x == 5 && y in 5..7) && !(x == 6 && y == 7) && !(x == 7 && y in 5..7)
 
-    fun adjacentTiles(initial: Pair<Int, Int>, diagonal: Boolean = false): List<Pair<Int, Int>> {
+    private fun adjacentTiles(
+        initial: Pair<Int, Int>,
+        other: Pair<Int, Int>,
+        diagonal: Boolean = false
+    ): List<Pair<Int, Int>> {
         val ret = mutableListOf<Pair<Int, Int>>()
         fun add(r: Int, c: Int) {
-            if (withinBounds(r, c)) ret.add(Pair(r, c))
+            if (withinBounds(r, c) && !(initial.first == other.first && initial.second == other.second)) ret.add(
+                Pair(
+                    r,
+                    c
+                )
+            )
         }
         val (r, c) = initial
         add(r + 1, c)
@@ -60,67 +58,59 @@ class A3 {
         return ret
     }
 
-    fun adjacentStates(state: State): List<State> {
-        val adj = mutableListOf<State>()
-        val (robot, bull) = state
+    // all actions the bull can take if the robot is in a tile and bull's current position
+    fun bullActions(robot: Pair<Int, Int>, bull: Pair<Int, Int>): List<State> {
+        val ret = mutableListOf<State>()
+        if (manhattan(robot, bull) <= 5) {
+            adjacentTiles(bull, robot).filter { manhattan(it, robot) <= manhattan(bull, robot) }
+                .map { allStates[robot.first][robot.second][it.first][it.second] }.forEach { ret.add(it) }
+        } else adjacentTiles(bull, robot).map { allStates[robot.first][robot.second][it.first][it.second] }
+            .forEach { ret.add(it) }
 
-        val adjRob = adjacentTiles(robot, true)
-
-        for (r in adjRob) {
-            if (manhattan(r, bull) <= 5) {
-                adjacentTiles(bull).filter { manhattan(it, r) <= manhattan(r, bull) }.forEach { adj.add(State(r, it)) }
-            } else {
-                adjacentTiles(bull).forEach { adj.add(State(r, it)) }
-            }
-        }
-        return adj.toList()
+        return ret.toList()
     }
 
-    fun robotMoves(state:State) : Int = adjacentTiles(state.robot,true).size
-    fun bullMoves(state:State) : Int {
-        if (manhattan(state.robot,state.bull) <= 5) return adjacentTiles(state.bull).filter { manhattan(it,state.robot) <= manhattan(state.robot,state.bull) }.size
-        return adjacentTiles(state.bull).size
-    }
+    private fun isGoalState(s: State): Boolean = (s.bull.first == 6) && (s.bull.second == 6)
 
-    fun isGoalState(s:State) :Boolean = (s.bull.first == 6) && (s.bull.first == 6)
-
-
-    fun calculateBellman(start: State) {
-        for (i in 0..<100) {
-            val visited = StateSet().also { it.add(start) }
-            val queue: Queue<State> = LinkedList()
-            while (queue.isNotEmpty()) {
-                val cur = queue.remove()
-                adjacentStates(cur).filter { !visited.contains(it) }.forEach {
-                    visited.add(it)
-                    queue.add(it)
+    fun calculateBellman(robotStart: Pair<Int, Int>, bullStart: Pair<Int, Int>): Double {
+        var tval: Double? = null
+        for (i in 0..<10000) {
+            for (rx in 0..12) {
+                for (ry in 0..12) {
+                    for (bx in 0..12) {
+                        for (by in 0..12) {
+                            val cur = allStates[rx][ry][bx][by]
+                            if (!isGoalState(cur) && withinBounds(rx, ry) && withinBounds(
+                                    bx,
+                                    by
+                                ) && !(rx == bx && ry == by)
+                            ) {
+                                val actions = adjacentTiles(
+                                    cur.robot,
+                                    cur.bull,
+                                    true
+                                )
+                                cur.value = actions.mapNotNull { action ->
+                                    if (bullActions(action, cur.bull).any { it.value != null }) {
+                                        val t = 1.0 / (bullActions(action, cur.bull).filter { it.value != null }.size)
+                                        bullActions(action, cur.bull).filter { it.value != null }
+                                            .sumOf { sPrime -> t * (1 + sPrime.value!!) }
+                                    } else null
+                                }.minOrNull()
+                            }
+                        }
+                    }
                 }
-
-                val (rx,ry) = cur.robot
-                val (bx,by) = cur.bull
-                val bm = bellman(cur) //.also { it.add(cur.value) }
-                allStates[rx][ry][bx][by].value = if (bm.filterNotNull().isEmpty()) null else bm.filterNotNull().min()
             }
+            println(allStates[robotStart.first][robotStart.second][bullStart.first][bullStart.second].value)
+            if (tval != null && abs(tval - allStates[robotStart.first][robotStart.second][bullStart.first][bullStart.second].value!!) < 0.0001) return allStates[robotStart.first][robotStart.second][bullStart.first][bullStart.second].value!!
+            else tval = allStates[robotStart.first][robotStart.second][bullStart.first][bullStart.second].value
+
         }
+        return allStates[robotStart.first][robotStart.second][bullStart.first][bullStart.second].value!!
     }
-
-    private fun t(s:State, sp:State) : Double{
-        return (1.0/robotMoves(s)) * (1.0/bullMoves(sp)) //might not be sp
-    }
-
-    private fun r(s:State) = if (s.bull.first == 6 && s.bull.second == 6) 0 else 1
-
-    private fun bellman(s:State) :MutableList<Double?>{
-        val ret = mutableListOf<Double?>()
-        adjacentStates(s).forEach { sp ->
-            if (sp.value == null) ret.add(null)
-            else ret.add(t(s,sp) * (r(sp) + sp.value!!))
-        }
-        return ret
-    }
-
 }
 
 fun main() {
-    A3().adjacentStates(A3.State(Pair(3, 3), Pair(1, 1)))
+    println(A3().calculateBellman(Pair(12, 12), Pair(0, 0)))
 }
